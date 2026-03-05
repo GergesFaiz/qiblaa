99b6af8fbd650c0509009079b1a70beb85fe07ef
gero_ <gergesfaiz3@gmail.com>
2026-03-05 15:49:31 +0200

    Sebha Screen Hadeth Screen

--- lib/ui/home/tabs/Ahadeth/hadethDetailsScreen.dart
+++ lib/ui/home/tabs/Ahadeth/hadethDetailsScreen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qiblaa/ui/home/tabs/Ahadeth/hadeth.dart';
 import 'package:qiblaa/ui/home/tabs/Ahadeth/hadethArgs.dart';
